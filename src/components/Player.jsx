1011e95bee40592b17b6b1392dfdb7293f130f7c
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-07-28 00:32:14 -0400

    Implemented 2 way binding and save the input field for key strokes with onChange

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,34 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditingState = () => {
-    setIsEditing(isEditing => !isEditing);
+    setIsEditing((isEditing) => !isEditing);
   };
 
-  let playerName = <span className="player-name">{name}</span>;
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
 
-
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name}/>;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditingState}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button onClick={handleEditingState}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
     </li>
   );
 }
