5269b13b8df0e78030a63c5fb66db01e44910243
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-08-11 01:13:16 -0400

    Added the option to restart the match successfully

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   const activePlayer = derivedPlayerState(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -73,6 +73,10 @@ function App() {
     });
   };
 
+  const handleRestart = () =>{
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -88,7 +92,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner = {winner}/>}
+        {(winner || hasDraw) && <GameOver winner={winner}  onRestart={handleRestart}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
